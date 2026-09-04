1251e2b6bae9933bc70243d8a330408a0e84af59
David Amofa <your_damofa002@st.ug.edu.gh>
2026-09-04 23:55:21 +0000

    Add Abstract Classes and Method task

---
+++ Abstractshapes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double r)
+    {
+        Radius = r;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double w, double h)
+    {
+        Width = w;
+        Height = h;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+        Rectangle rect = new Rectangle(4, 6);
+
+        Console.WriteLine("Circle area: " + circle.GetArea());
+        Console.WriteLine("Rectangle area: " + rect.GetArea());
+    }
+}

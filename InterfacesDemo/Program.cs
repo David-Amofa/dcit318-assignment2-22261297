83f4b072c543294e9df4e66612c3803865501a1a
David Amofa <your_damofa002@st.ug.edu.gh>
2026-09-05 00:22:18 +0000

    Add Interfaces task

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Dci318Assignment2
+{
+    // Define the interface
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    // Car implements IMovable
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle implements IMovable
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    // Entry point
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
